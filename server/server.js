8c03426cd52b650ab018fc18bc8014a5489f68c3
pspeiser <pat@patspeiser.com>
2016-11-05 18:15:26 -0400

    stuff

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ const Event = require('./db').models.Event;
 
 if (process.env.SYNC){
 	db.sync()
-		.then(function(){
-		})
-		.then(function(){
-			console.log('db synced');
-			server.listen(port, function(){
-				console.log('listening on port... '+ port);
-			});
-		});
+	.then(function(){
+	})
+	.then(function(){
+		console.log('db synced');
+		
+	});
 }
 
+server.listen(port, function(){
+	console.log('listening on port... '+ port);
+});
